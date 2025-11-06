20ec85e955e0ba4abf11dc7584b0db321e93cba9
schismatiic <leonardojofre2003@gmail.com>
2025-11-06 12:10:24 -0300

    Creación del logo y navbar
    Primer componente "Navbar" implementado. También agregué un logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <></>;
+  const [tab, setTab] = useState("home");
+
+  return (
+    <>
+      <Navbar setTab={setTab}></Navbar>
+    </>
+  );
 }
 
 export default App;
