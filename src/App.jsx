818c9b06c93ac9246d3594aa580b4c37c3ccb6c5
ELBA21 <benjaminalberto.martinez@alumnos.ulagos.cl>
2025-11-06 17:23:49 -0300

    Test Menores: Respuesta efectiva

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import healthcheck from "./services/health";
 function App() {
   const [tab, setTab] = useState("home");
+
+  console.log("[DEBUG]: Probando Healthcheck");
+  healthcheck();
+
+  console.log("Healthcheck Ejecutado");
 
   return (
     <>
--- src/services/health.js
+++ src/services/health.js
@@ -4,10 +4,11 @@ async function healthcheck() {
   try {
     const response = await fetch(api);
     if (!response.ok) {
+      console.log("[DEBUG] ERROR");
       throw new Error(`Response status: ${response.status}`);
     }
     const result = await response.text();
-    console.log("healthcheck", result);
+    console.log("healthcheck aaaa", result);
     return result;
   } catch (err) {
     console.log("[DEBUG]: health.js error en healthcheck");
