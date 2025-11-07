6eb849fc93091cfe6d6fd5cf7069023ca420d927
schismatiic <leonardojofre2003@gmail.com>
2025-11-07 15:00:06 -0300

    Cambios visuales en EventCard

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,11 +1,32 @@
 import "./styles/event-card.css";
-function EventCard({ title, image, description, price }) {
+function EventCard({ image, location, title, date, price }) {
   return (
     <div className="event-card">
-      <h3>{title}</h3>
       <img src={image} alt={title} />
-      <p>{description}</p>
-      <p>${price}</p>
+      <div className="event-card__info">
+        <h3>{title}</h3>
+        <div className="card-row">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>map-marker</title>
+            <path d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" />
+          </svg>
+          <h6>{location}</h6>
+        </div>
+        <div className="card-row">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>calendar-range</title>
+            <path d="M9,10H7V12H9V10M13,10H11V12H13V10M17,10H15V12H17V10M19,3H18V1H16V3H8V1H6V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V8H19V19Z" />
+          </svg>
+          <p>{date}</p>
+        </div>
+        <div className="card-row">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>ticket-confirmation-outline</title>
+            <path d="M22 10V6C22 4.89 21.1 4 20 4H4C2.9 4 2 4.89 2 6V10C3.11 10 4 10.9 4 12S3.11 14 2 14V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V14C20.9 14 20 13.1 20 12S20.9 10 22 10M20 8.54C18.81 9.23 18 10.53 18 12S18.81 14.77 20 15.46V18H4V15.46C5.19 14.77 6 13.47 6 12C6 10.5 5.2 9.23 4 8.54L4 6H20V8.54M11 15H13V17H11M11 11H13V13H11M11 7H13V9H11Z" />
+          </svg>
+          <p>${price}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,28 +4,20 @@ function Home() {
   return (
     <div className="event-container">
       <EventCard
+        image="https://i.redd.it/3ccvjqsymii91.png"
+        location="Estadio nacional - Ñuñoa"
         title="Radiohead"
-        image="https://upload.wikimedia.org/wikipedia/en/0/02/Radioheadkida.png"
         description="Kid A concert"
-        price="80"
-      ></EventCard>{" "}
+        date="05 de Diciembre - 07 de Diciembre"
+        price="80.000"
+      ></EventCard>
       <EventCard
+        image="https://i.redd.it/3ccvjqsymii91.png"
+        location="Estadio nacional - Ñuñoa"
         title="Radiohead"
-        image="https://upload.wikimedia.org/wikipedia/en/0/02/Radioheadkida.png"
         description="Kid A concert"
-        price="80"
-      ></EventCard>{" "}
-      <EventCard
-        title="Radiohead"
-        image="https://upload.wikimedia.org/wikipedia/en/0/02/Radioheadkida.png"
-        description="Kid A concert"
-        price="80"
-      ></EventCard>{" "}
-      <EventCard
-        title="Radiohead"
-        image="https://upload.wikimedia.org/wikipedia/en/0/02/Radioheadkida.png"
-        description="Kid A concert"
-        price="80"
+        date="05 de Diciembre - 07 de Diciembre"
+        price="80.000"
       ></EventCard>
     </div>
   );
