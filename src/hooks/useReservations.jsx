773ad7375613baff031747535275208865d9b1e7
Nurodayo <cianuroicecream@gmail.com>
2025-11-09 18:41:15 -0300

    added post hook

--- src/hooks/useReservations.jsx
+++ src/hooks/useReservations.jsx
@@ -61,3 +61,42 @@ export function useDeleteReservation(id) {
   }, []);
   return { id, deleted, loading, error };
 }
+// Reservation body
+// {
+//   "event_id": "68f7b9d771fbcc686dd144e8",
+//   "items": [
+//     {
+//       "quantity": 2,
+//       "type": "General"
+//     }
+//   ]
+// }
+export function usePostReservation(reservation) {
+  const [id, setId] = useState("");
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    const response = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const post = api.post(reservation);
+        setData(post);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+        setId(data.reservation_id); //retorno el id de la reservacion recien creada
+      }
+    };
+
+    response();
+  }, []);
+  return { id, reservation, loading, error };
+}
