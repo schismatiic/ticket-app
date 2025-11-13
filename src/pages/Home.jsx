6ad3916a1b226425a375417984825d5c30b1f850
schismatiic <leonardojofre2003@gmail.com>
2025-11-12 21:30:40 -0300

    Sorpresa :0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import "./styles/home.css";
 import { useNavigate } from "react-router-dom";
 
 import { useEffect } from "react";
-import { useGetEvents } from "../hooks/useEvents"; 
+import { useGetEvents } from "../hooks/useEvents";
+
+import loadingGif from "../assets/linux-tux.gif";
 
 function Home() {
   const { getEvents, data, isLoading, error } = useGetEvents();
@@ -15,23 +17,34 @@ function Home() {
     getEvents();
   }, []);
 
-  if (isLoading) return <p>Cargando eventos...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-  if (!data) return <p>No hay datos todavía</p>;
+  if (isLoading)
+    return (
+      <div className="status-message">
+        {" "}
+        <p>Cargando eventos...</p>
+        <img src={loadingGif} alt="Cargando..." className="loading-gif" />
+      </div>
+    );
+  if (error)
+    return <div className="status-message">Error: {error.message}</div>;
+  if (!data) return <div className="status-message">No hay datos todavía</div>;
 
   const eventos = data.data;
 
   return (
     <div className="event-container">
-      {eventos.map(evento => (
-        <EventCard key={evento._id}
-          name={evento.name} 
+      {eventos.map((evento) => (
+        <EventCard
+          key={evento._id}
+          name={evento.name}
           category={evento.category}
-          date={evento.date}   
-          location={evento.location} 
-          image={evento.image} 
+          date={evento.date}
+          location={evento.location}
+          image={evento.image}
           tickets={evento.tickets}
-          onClick={() => navigate(`/event-detail/${evento._id}`, { state: {evento} })}
+          onClick={() =>
+            navigate(`/event-detail/${evento._id}`, { state: { evento } })
+          }
         ></EventCard>
       ))}
     </div>
