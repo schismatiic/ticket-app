3cc902ec72ed0d3e4b2c945a3538f7e2198405a3
Andres Hernandez <andresfernando.hernandez@alumnos.ulagos.cl>
2025-11-13 00:14:14 -0300

    Snapshot de mi trabajo actual

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,26 @@ import "./styles/home.css";
 //import { eventos } from "../services/eventosArreglo";
 import { useNavigate } from "react-router-dom";
 
-import { useEffect } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useGetEvents } from "../hooks/useEvents";
 
 import loadingGif from "../assets/linux-tux.gif";
 
+import SearchBar from "../components/SearchBar.jsx";
+
 function Home() {
   const { getEvents, data, isLoading, error } = useGetEvents();
+  const [busqueda, setBusqueda] = useState("");
   const navigate = useNavigate();
+
+  const eventos = data?.data || [];
+
+  const eventosFiltrados = useMemo(() => {
+    return eventos.filter(e =>
+      e.name.toLowerCase().includes(busqueda.toLowerCase())
+    );
+  }, [eventos, busqueda]);
+
 
   useEffect(() => {
     // al montar el componente, se llama a getEvents sin parámetros
@@ -29,24 +41,46 @@ function Home() {
     return <div className="status-message">Error: {error.message}</div>;
   if (!data) return <div className="status-message">No hay datos todavía</div>;
 
-  const eventos = data.data;
-
   return (
-    <div className="event-container">
-      {eventos.map((evento) => (
-        <EventCard
-          key={evento._id}
-          name={evento.name}
-          category={evento.category}
-          date={evento.date}
-          location={evento.location}
-          image={evento.image}
-          tickets={evento.tickets}
-          onClick={() =>
-            navigate(`/event-detail/${evento._id}`, { state: { evento } })
-          }
-        ></EventCard>
-      ))}
+    <div>
+      <br></br>
+      <SearchBar busqueda={busqueda} setBusqueda={setBusqueda}></SearchBar>
+
+      {busqueda ? (
+        <div className="event-container">
+          {eventosFiltrados.map((evento) => (
+            <EventCard
+              key={evento._id}
+              name={evento.name}
+              category={evento.category}
+              date={evento.date}
+              location={evento.location}
+              image={evento.image}
+              tickets={evento.tickets}
+              onClick={() =>
+                navigate(`/event-detail/${evento._id}`, { state: { evento } })
+              }
+            ></EventCard>
+          ))}
+        </div>
+      ) : (
+        <div className="event-container">
+          {eventos.map((evento) => (
+            <EventCard
+              key={evento._id}
+              name={evento.name}
+              category={evento.category}
+              date={evento.date}
+              location={evento.location}
+              image={evento.image}
+              tickets={evento.tickets}
+              onClick={() =>
+                navigate(`/event-detail/${evento._id}`, { state: { evento } })
+              }
+            ></EventCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
